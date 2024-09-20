59b864a6be9dbcce30c36012b1bf7113362b8a77
bob-ashforth-sp <bob.ashforth@sailpoint.com>
2024-09-20 06:50:54 -0500

    NEDEVOPS-2993: Deleting commented-out lines, not needed

--- variables.tf
+++ variables.tf
@@ -55,14 +55,6 @@ variable "enable_organization_runners" {
   default     = true
 }
 
-#variable "github_app" {
-#  description = "GitHub app parameters, see your github app. Ensure the key is the base64-encoded `.pem` file (the output of `base64 app.private-key.pem`, not the content of `private-key.pem`)."
-#  type = object({
-#    key_base64     = string
-#    webhook_secret = string
-#  })
-#}
-
 variable "scale_down_schedule_expression" {
   description = "Scheduler expression to check every x for scale down."
   type        = string
